52c03a353664185c81481a472a6968d225679213
merge-simpson <letsdev@kakao.com>
2023-04-22 02:57:41 +0900

    refactor(sign in): rename property of sign in response dto

--- src/main/java/com/example/demo/member/api/dto/AuthenticationDto.java
+++ src/main/java/com/example/demo/member/api/dto/AuthenticationDto.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
+import lombok.Builder;
 
 public final class AuthenticationDto {
     public record SignInRequestDto (
@@ -14,8 +15,9 @@ public final class AuthenticationDto {
             String rawPassword
     ) {}
 
+    @Builder
     public record SignInResponseDto (
-            @JsonInclude(Include.NON_DEFAULT) Boolean requires2Fa,
+            @JsonInclude(Include.NON_DEFAULT) Boolean requiresMfa,
             @JsonInclude(Include.NON_EMPTY) String token
     ) {}
 }
