8223d93e3d6d0ba97eda93a65c1515a1fd765e19
merge-simpson <letsdev@kakao.com>
2023-04-22 02:58:03 +0900

    feat(sign in): create sign-in api

--- src/main/java/com/example/demo/member/service/DefaultAuthService.java
+++ src/main/java/com/example/demo/member/service/DefaultAuthService.java
@@ -2,8 +2,12 @@ package com.example.demo.member.service;
 
 import com.example.demo.member.api.dto.AuthenticationDto.SignInRequestDto;
 import com.example.demo.member.api.dto.AuthenticationDto.SignInResponseDto;
+import com.example.demo.util.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,9 +15,21 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class DefaultAuthService implements AuthService {
 
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider jwtTokenProvider;
+
     @Override
     public SignInResponseDto signIn(SignInRequestDto body) {
-        // TODO 이곳에서 인증을 수행
-        return null;
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(body.email(), body.rawPassword());
+
+        Authentication authentication = authenticationManager.authenticate(authenticationToken);
+
+        String token = jwtTokenProvider.generate(body.email());
+
+        return SignInResponseDto.builder()
+                .requiresMfa(false)
+                .token(token)
+                .build();
     }
 }
